627e30e9c070757e52e3df49d27dea1d3b364e2b
ajy5119 <ajy5119@202-WD43.up.ist.local>
2016-10-05 15:12:44 -0400

    Initial Commit

---
+++ src/appp/Appp.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appp;
+
+/**
+ *
+ * @author ajy5119
+ */
+public class Appp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainFrame mFrame = new MainFrame();
+    }
+    
+}
